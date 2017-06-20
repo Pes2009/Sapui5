d6bc5662050e76cfa02dd5324e93c2a19cae3e2e
Martin Madej <martin.madej@incataconsulting.com>
2017-06-20 14:04:51 +0000

    added all

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,9 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function(Controller) {
+	"use strict";
+	this.getView();
+	return Controller.extend("hcpbook.chp07.demoExample0701.controller.App", {
+
+	});
+});
