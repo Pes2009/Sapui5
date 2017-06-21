3d01e90f3f2c44ce501b893c0b233657b040676f
Martin Madej <martin.madej@incataconsulting.com>
2017-06-21 08:28:03 +0000

    update odata

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,10 +1,11 @@
-sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function(Controller) {
-	"use strict";
-	this.getView();
-	return Controller.extend("hcpbook.chp07.demoExample0701.controller.App", {
-	  /**
+sap.ui.core.mvc.Controller.extend("hcpbook.chp07.demoExample0701.App", {
+  /**
+   * Callback method used to initialize the controller.
+   */
+  onInit: function() {
+  },
+  
+  /**
    * Event handler method used to handle a customer selection by the user.
    */
   onCustomerSelect: function(oEvent) {
@@ -16,5 +17,29 @@ sap.ui.define([
     tabOrders.bindRows(sSelRowCtx + "/Orders");
     tabOrders.setVisible(true);
   }
+});
+
+
+
+/*
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function(Controller) {
+	"use strict";
+	this.getView();
+	return Controller.extend("hcpbook.chp07.demoExample0701.controller.App", {
+	  /**
+   * Event handler method used to handle a customer selection by the user.
+   
+  onCustomerSelect: function(oEvent) {
+    // Determine the selected customer:
+    var sSelRowCtx = oEvent.getParameter("rowContext");
+      
+    // Sync up the customer's orders:
+    var tabOrders = this.getView().byId("tabOrders");
+    tabOrders.bindRows(sSelRowCtx + "/Orders");
+    tabOrders.setVisible(true);
+  }
 	});
 });
+*/
