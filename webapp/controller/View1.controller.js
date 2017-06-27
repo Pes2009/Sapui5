01226f50cd6215eff5e0c3859c83ef7cc6876ead
Martin Madej <martin.madej@incataconsulting.com>
2017-06-27 08:45:55 +0000

    version 1.0

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -3,7 +3,14 @@ sap.ui.define([
 ], function(Controller) {
 	"use strict";
 
-	return Controller.extend("HelloWorld.controller.View1", {
-
+return Controller.extend("HelloWorld.controller.View1", {
+	handleListItemPress: function (evt) {
+ 	var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+ 	var selectedProductId = evt.getSource().getBindingContext().getProperty("ProductID");
+	oRouter.navTo("detail", {
+		productId: selectedProductId
 	});
+}
+});
+
 });
